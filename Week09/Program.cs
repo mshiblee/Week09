caa545bd2e7d0f681f676aead0ae98b53d2583bb
Muhammad Shiblee <m.shiblee@outlook.com>
2019-07-10 07:33:55 -0400

    created base empty form

--- Week09/Program.cs
+++ Week09/Program.cs
@@ -16,7 +16,7 @@ namespace Week09
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
